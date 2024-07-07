05709de3677dbde9eb209e8111fa82652f4ef054
اکبرفرهمند <137598362+farahmandakbar26@users.noreply.github.com>
2024-07-08 01:49:17 +0330

    Initialize github.dev/farahmandakbar26/desktop/blob

---
+++ app/src/ui/preferences/advanced.tsx
@@ -0,0 +1,127 @@
+import * as React from 'react'
+import { DialogContent } from '../dialog'
+import { Checkbox, CheckboxValue } from '../lib/checkbox'
+import { LinkButton } from '../lib/link-button'
+import { SamplesURL } from '../../lib/stats'
+import { isWindowsOpenSSHAvailable } from '../../lib/ssh/ssh'
+
+interface IAdvancedPreferencesProps {
+  readonly useWindowsOpenSSH: boolean
+  readonly optOutOfUsageTracking: boolean
+  readonly repositoryIndicatorsEnabled: boolean
+  readonly onUseWindowsOpenSSHChanged: (checked: boolean) => void
+  readonly onOptOutofReportingChanged: (checked: boolean) => void
+  readonly onRepositoryIndicatorsEnabledChanged: (enabled: boolean) => void
+}
+
+interface IAdvancedPreferencesState {
+  readonly optOutOfUsageTracking: boolean
+  readonly canUseWindowsSSH: boolean
+}
+
+export class Advanced extends React.Component<
+  IAdvancedPreferencesProps,
+  IAdvancedPreferencesState
+> {
+  public constructor(props: IAdvancedPreferencesProps) {
+    super(props)
+
+    this.state = {
+      optOutOfUsageTracking: this.props.optOutOfUsageTracking,
+      canUseWindowsSSH: false,
+    }
+  }
+
+  public componentDidMount() {
+    this.checkSSHAvailability()
+  }
+
+  private async checkSSHAvailability() {
+    this.setState({ canUseWindowsSSH: await isWindowsOpenSSHAvailable() })
+  }
+
+  private onReportingOptOutChanged = (
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    const value = !event.currentTarget.checked
+
+    this.setState({ optOutOfUsageTracking: value })
+    this.props.onOptOutofReportingChanged(value)
+  }
+
+  private onRepositoryIndicatorsEnabledChanged = (
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    this.props.onRepositoryIndicatorsEnabledChanged(event.currentTarget.checked)
+  }
+
+  private onUseWindowsOpenSSHChanged = (
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    this.props.onUseWindowsOpenSSHChanged(event.currentTarget.checked)
+  }
+
+  private reportDesktopUsageLabel() {
+    return (
+      <span>
+        Help GitHub Desktop improve by submitting{' '}
+        <LinkButton uri={SamplesURL}>usage stats</LinkButton>
+      </span>
+    )
+  }
+
+  public render() {
+    return (
+      <DialogContent>
+        <div className="advanced-section">
+          <h2>Background updates</h2>
+          <Checkbox
+            label="Periodically fetch and refresh status of all repositories"
+            value={
+              this.props.repositoryIndicatorsEnabled
+                ? CheckboxValue.On
+                : CheckboxValue.Off
+            }
+            onChange={this.onRepositoryIndicatorsEnabledChanged}
+          />
+          <p className="git-settings-description">
+            Allows the display of up-to-date status indicators in the repository
+            list. Disabling this may improve performance with many repositories.
+          </p>
+        </div>
+        {this.renderSSHSettings()}
+        <div className="advanced-section">
+          <h2>Usage</h2>
+          <Checkbox
+            label={this.reportDesktopUsageLabel()}
+            value={
+              this.state.optOutOfUsageTracking
+                ? CheckboxValue.Off
+                : CheckboxValue.On
+            }
+            onChange={this.onReportingOptOutChanged}
+          />
+        </div>
+      </DialogContent>
+    )
+  }
+
+  private renderSSHSettings() {
+    if (!this.state.canUseWindowsSSH) {
+      return null
+    }
+
+    return (
+      <div className="advanced-section">
+        <h2>SSH</h2>
+        <Checkbox
+          label="Use system OpenSSH (recommended)"
+          value={
+            this.props.useWindowsOpenSSH ? CheckboxValue.On : CheckboxValue.Off
+          }
+          onChange={this.onUseWindowsOpenSSHChanged}
+        />
+      </div>
+    )
+  }
+}
