f15771f56b4c123414471c79816fc539d9a6da06
اکبرفرهمند <137598362+farahmandakbar26@users.noreply.github.com>
2024-07-08 01:39:04 +0330

    Initialize releases/3.2.2-test8

---
+++ app/src/ui/preferences/appearance.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react'
+import {
+  ApplicationTheme,
+  supportsSystemThemeChanges,
+  getCurrentlyAppliedTheme,
+  ICustomTheme,
+} from '../lib/application-theme'
+import { Row } from '../lib/row'
+import { DialogContent } from '../dialog'
+import { CustomThemeSelector } from './custom-theme-selector'
+import { enableHighContrastTheme } from '../../lib/feature-flag'
+import { RadioGroup } from '../lib/radio-group'
+import { encodePathAsUrl } from '../../lib/path'
+
+interface IAppearanceProps {
+  readonly selectedTheme: ApplicationTheme
+  readonly customTheme?: ICustomTheme
+  readonly onSelectedThemeChanged: (theme: ApplicationTheme) => void
+  readonly onCustomThemeChanged: (theme: ICustomTheme) => void
+}
+
+interface IAppearanceState {
+  readonly selectedTheme: ApplicationTheme | null
+}
+
+export class Appearance extends React.Component<
+  IAppearanceProps,
+  IAppearanceState
+> {
+  public constructor(props: IAppearanceProps) {
+    super(props)
+
+    const usePropTheme =
+      props.selectedTheme !== ApplicationTheme.System ||
+      supportsSystemThemeChanges()
+
+    this.state = { selectedTheme: usePropTheme ? props.selectedTheme : null }
+
+    if (!usePropTheme) {
+      this.initializeSelectedTheme()
+    }
+  }
+
+  public async componentDidUpdate(prevProps: IAppearanceProps) {
+    if (prevProps.selectedTheme === this.props.selectedTheme) {
+      return
+    }
+
+    const usePropTheme =
+      this.props.selectedTheme !== ApplicationTheme.System ||
+      supportsSystemThemeChanges()
+
+    const selectedTheme = usePropTheme
+      ? this.props.selectedTheme
+      : await getCurrentlyAppliedTheme()
+
+    this.setState({ selectedTheme })
+  }
+
+  private initializeSelectedTheme = async () => {
+    const selectedTheme = await getCurrentlyAppliedTheme()
+    this.setState({ selectedTheme })
+  }
+
+  private onSelectedThemeChanged = (theme: ApplicationTheme) => {
+    this.props.onSelectedThemeChanged(theme)
+  }
+
+  private onCustomThemeChanged = (theme: ICustomTheme) => {
+    this.props.onCustomThemeChanged(theme)
+  }
+
+  public renderThemeSwatch = (theme: ApplicationTheme) => {
+    const darkThemeImage = encodePathAsUrl(__dirname, 'static/ghd_dark.svg')
+    const lightThemeImage = encodePathAsUrl(__dirname, 'static/ghd_light.svg')
+
+    switch (theme) {
+      case ApplicationTheme.Light:
+        return (
+          <span>
+            <img src={lightThemeImage} alt="" />
+            <span className="theme-value-label">Light</span>
+          </span>
+        )
+      case ApplicationTheme.Dark:
+        return (
+          <span>
+            <img src={darkThemeImage} alt="" />
+            <span className="theme-value-label">Dark</span>
+          </span>
+        )
+      case ApplicationTheme.System:
+        /** Why three images? The system theme swatch uses the first image
+         * positioned relatively to get the label container size and uses the
+         * second and third positioned absolutely over first and third one
+         * clipped in half to render a split dark and light theme swatch. */
+        return (
+          <span>
+            <span className="system-theme-swatch">
+              <img src={lightThemeImage} alt="" />
+              <img src={lightThemeImage} alt="" />
+              <img src={darkThemeImage} alt="" />
+            </span>
+            <span className="theme-value-label">System</span>
+          </span>
+        )
+      case ApplicationTheme.HighContrast:
+        return (
+          <span>
+            <img src={darkThemeImage} alt="" />
+            <span className="theme-value-label">High Contrast</span>
+          </span>
+        )
+    }
+  }
+
+  public render() {
+    const { selectedTheme } = this.state
+
+    if (selectedTheme == null) {
+      return (
+        <DialogContent>
+          <Row>Loading system theme</Row>
+        </DialogContent>
+      )
+    }
+
+    const themes = [
+      ApplicationTheme.Light,
+      ApplicationTheme.Dark,
+      ...(supportsSystemThemeChanges() ? [ApplicationTheme.System] : []),
+      ...(enableHighContrastTheme() ? [ApplicationTheme.HighContrast] : []),
+    ]
+
+    return (
+      <DialogContent>
+        <h2 id="theme-heading">Theme</h2>
+
+        <RadioGroup<ApplicationTheme>
+          ariaLabelledBy="theme-heading"
+          className="theme-selector"
+          selectedKey={selectedTheme}
+          radioButtonKeys={themes}
+          onSelectionChanged={this.onSelectedThemeChanged}
+          renderRadioButtonLabelContents={this.renderThemeSwatch}
+        />
+
+        <Row>
+          <CustomThemeSelector
+            onCustomThemeChanged={this.onCustomThemeChanged}
+            selectedTheme={selectedTheme}
+            customTheme={this.props.customTheme}
+          />
+        </Row>
+      </DialogContent>
+    )
+  }
+}
