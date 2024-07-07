2e08c516bd2a7b91d8d0c09022868bda737bf90d
اکبرفرهمند <137598362+farahmandakbar26@users.noreply.github.com>
2024-07-08 01:48:33 +0330

    Initialize retain-focus

---
+++ app/src/ui/clone-repository/group-repositories.ts
@@ -0,0 +1,75 @@
+import { IAPIRepository } from '../../lib/api'
+import { IFilterListGroup, IFilterListItem } from '../lib/filter-list'
+import { OcticonSymbolType } from '../octicons'
+import * as OcticonSymbol from '../octicons/octicons.generated'
+import { entries, groupBy } from 'lodash'
+import { caseInsensitiveEquals, compare } from '../../lib/compare'
+
+/** The identifier for the "Your Repositories" grouping. */
+export const YourRepositoriesIdentifier = 'your-repositories'
+
+export interface ICloneableRepositoryListItem extends IFilterListItem {
+  /** The identifier for the item. */
+  readonly id: string
+
+  /** The search text. */
+  readonly text: ReadonlyArray<string>
+
+  /** The name of the repository. */
+  readonly name: string
+
+  /** The icon for the repo. */
+  readonly icon: OcticonSymbolType
+
+  /** The clone URL. */
+  readonly url: string
+
+  /** Whether or not the repository is archived */
+  readonly archived?: boolean
+}
+
+function getIcon(gitHubRepo: IAPIRepository): OcticonSymbolType {
+  if (gitHubRepo.private) {
+    return OcticonSymbol.lock
+  }
+  if (gitHubRepo.fork) {
+    return OcticonSymbol.repoForked
+  }
+
+  return OcticonSymbol.repo
+}
+
+const toListItems = (repositories: ReadonlyArray<IAPIRepository>) =>
+  repositories
+    .map<ICloneableRepositoryListItem>(repo => ({
+      id: repo.html_url,
+      text: [`${repo.owner.login}/${repo.name}`],
+      url: repo.clone_url,
+      name: repo.name,
+      icon: getIcon(repo),
+      archived: repo.archived,
+    }))
+    .sort((x, y) => compare(x.name, y.name))
+
+export function groupRepositories(
+  repositories: ReadonlyArray<IAPIRepository>,
+  login: string
+): ReadonlyArray<IFilterListGroup<ICloneableRepositoryListItem>> {
+  const groups = groupBy(repositories, x =>
+    caseInsensitiveEquals(x.owner.login, login)
+      ? YourRepositoriesIdentifier
+      : x.owner.login
+  )
+
+  return entries(groups)
+    .map(([identifier, repos]) => ({ identifier, items: toListItems(repos) }))
+    .sort((x, y) => {
+      if (x.identifier === YourRepositoriesIdentifier) {
+        return -1
+      } else if (y.identifier === YourRepositoriesIdentifier) {
+        return 1
+      } else {
+        return compare(x.identifier, y.identifier)
+      }
+    })
+}
---
+++ app/src/ui/preferences/appearance.tsx
@@ -0,0 +1,133 @@
+import * as React from 'react'
+import {
+  ApplicationTheme,
+  supportsSystemThemeChanges,
+  getCurrentlyAppliedTheme,
+} from '../lib/application-theme'
+import { Row } from '../lib/row'
+import { DialogContent } from '../dialog'
+import { RadioGroup } from '../lib/radio-group'
+import { encodePathAsUrl } from '../../lib/path'
+
+interface IAppearanceProps {
+  readonly selectedTheme: ApplicationTheme
+  readonly onSelectedThemeChanged: (theme: ApplicationTheme) => void
+}
+
+interface IAppearanceState {
+  readonly selectedTheme: ApplicationTheme | null
+}
+
+export class Appearance extends React.Component<
+  IAppearanceProps,
+  IAppearanceState
+> {
+  public constructor(props: IAppearanceProps) {
+    super(props)
+
+    const usePropTheme =
+      props.selectedTheme !== ApplicationTheme.System ||
+      supportsSystemThemeChanges()
+
+    this.state = { selectedTheme: usePropTheme ? props.selectedTheme : null }
+
+    if (!usePropTheme) {
+      this.initializeSelectedTheme()
+    }
+  }
+
+  public async componentDidUpdate(prevProps: IAppearanceProps) {
+    if (prevProps.selectedTheme === this.props.selectedTheme) {
+      return
+    }
+
+    const usePropTheme =
+      this.props.selectedTheme !== ApplicationTheme.System ||
+      supportsSystemThemeChanges()
+
+    const selectedTheme = usePropTheme
+      ? this.props.selectedTheme
+      : await getCurrentlyAppliedTheme()
+
+    this.setState({ selectedTheme })
+  }
+
+  private initializeSelectedTheme = async () => {
+    const selectedTheme = await getCurrentlyAppliedTheme()
+    this.setState({ selectedTheme })
+  }
+
+  private onSelectedThemeChanged = (theme: ApplicationTheme) => {
+    this.props.onSelectedThemeChanged(theme)
+  }
+
+  public renderThemeSwatch = (theme: ApplicationTheme) => {
+    const darkThemeImage = encodePathAsUrl(__dirname, 'static/ghd_dark.svg')
+    const lightThemeImage = encodePathAsUrl(__dirname, 'static/ghd_light.svg')
+
+    switch (theme) {
+      case ApplicationTheme.Light:
+        return (
+          <span>
+            <img src={lightThemeImage} alt="" />
+            <span className="theme-value-label">Light</span>
+          </span>
+        )
+      case ApplicationTheme.Dark:
+        return (
+          <span>
+            <img src={darkThemeImage} alt="" />
+            <span className="theme-value-label">Dark</span>
+          </span>
+        )
+      case ApplicationTheme.System:
+        /** Why three images? The system theme swatch uses the first image
+         * positioned relatively to get the label container size and uses the
+         * second and third positioned absolutely over first and third one
+         * clipped in half to render a split dark and light theme swatch. */
+        return (
+          <span>
+            <span className="system-theme-swatch">
+              <img src={lightThemeImage} alt="" />
+              <img src={lightThemeImage} alt="" />
+              <img src={darkThemeImage} alt="" />
+            </span>
+            <span className="theme-value-label">System</span>
+          </span>
+        )
+    }
+  }
+
+  public render() {
+    const { selectedTheme } = this.state
+
+    if (selectedTheme == null) {
+      return (
+        <DialogContent>
+          <Row>Loading system theme</Row>
+        </DialogContent>
+      )
+    }
+
+    const themes = [
+      ApplicationTheme.Light,
+      ApplicationTheme.Dark,
+      ...(supportsSystemThemeChanges() ? [ApplicationTheme.System] : []),
+    ]
+
+    return (
+      <DialogContent>
+        <h2 id="theme-heading">Theme</h2>
+
+        <RadioGroup<ApplicationTheme>
+          ariaLabelledBy="theme-heading"
+          className="theme-selector"
+          selectedKey={selectedTheme}
+          radioButtonKeys={themes}
+          onSelectionChanged={this.onSelectedThemeChanged}
+          renderRadioButtonLabelContents={this.renderThemeSwatch}
+        />
+      </DialogContent>
+    )
+  }
+}
