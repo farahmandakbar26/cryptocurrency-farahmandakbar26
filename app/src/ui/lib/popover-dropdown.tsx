788823d3aacc46fde40cc86d889540d1386ea06d
اکبرفرهمند <137598362+farahmandakbar26@users.noreply.github.com>
2024-07-08 01:40:59 +0330

    Initialize generic-popover-dropdown

---
+++ app/src/ui/lib/popover-dropdown.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react'
+import { Popover, PopoverAnchorPosition, PopoverDecoration } from './popover'
+import { Octicon } from '../octicons'
+import * as OcticonSymbol from '../octicons/octicons.generated'
+import classNames from 'classnames'
+
+const maxPopoverContentHeight = 500
+
+export type PopoverDropdownAnchorProps = {
+  readonly onClick?: () => void
+  readonly onAnchorRef?: (ref: HTMLElement | null) => void
+}
+
+interface IPopoverDropdownProps {
+  readonly className?: string
+  readonly contentTitle: string
+  readonly renderAnchor: (props: PopoverDropdownAnchorProps) => JSX.Element
+}
+
+interface IPopoverDropdownState {
+  readonly showPopover: boolean
+}
+
+/**
+ * A dropdown component for displaying a dropdown button that opens
+ * a popover to display contents relative to the button content.
+ */
+export class PopoverDropdown extends React.Component<
+  IPopoverDropdownProps,
+  IPopoverDropdownState
+> {
+  private invokeButtonRef: HTMLElement | null = null
+
+  public constructor(props: IPopoverDropdownProps) {
+    super(props)
+
+    this.state = {
+      showPopover: false,
+    }
+  }
+
+  private onInvokeButtonRef = (buttonRef: HTMLElement | null) => {
+    this.invokeButtonRef = buttonRef
+  }
+
+  private togglePopover = () => {
+    this.setState({ showPopover: !this.state.showPopover })
+  }
+
+  public closePopover = () => {
+    this.setState({ showPopover: false })
+  }
+
+  private renderPopover() {
+    if (!this.state.showPopover) {
+      return
+    }
+
+    const { contentTitle } = this.props
+
+    return (
+      <Popover
+        className="popover-dropdown-popover"
+        anchor={this.invokeButtonRef}
+        anchorPosition={PopoverAnchorPosition.BottomLeft}
+        maxHeight={maxPopoverContentHeight}
+        decoration={PopoverDecoration.Balloon}
+        onClickOutside={this.closePopover}
+        aria-labelledby="popover-dropdown-header"
+      >
+        <div className="popover-dropdown-wrapper">
+          <div className="popover-dropdown-header">
+            <span id="popover-dropdown-header">{contentTitle}</span>
+
+            <button
+              className="close"
+              onClick={this.closePopover}
+              aria-label="close"
+            >
+              <Octicon symbol={OcticonSymbol.x} />
+            </button>
+          </div>
+          <div className="popover-dropdown-content">{this.props.children}</div>
+        </div>
+      </Popover>
+    )
+  }
+
+  public render() {
+    const { className, renderAnchor } = this.props
+    const cn = classNames('popover-dropdown-component', className)
+
+    return (
+      <div className={cn}>
+        {renderAnchor({
+          onClick: this.togglePopover,
+          onAnchorRef: this.onInvokeButtonRef,
+        })}
+        {this.renderPopover()}
+      </div>
+    )
+  }
+}
